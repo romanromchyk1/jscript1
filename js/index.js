029cbe72d6e3796cd4df97275fc66dede74d9272
Roman Khoronzhak <romix.price.3xd@gmail.com>
2016-12-06 17:48:09 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// alert('Hello world!');
+// document.write('<h1>Hello world!</h1>');
+// console.log('Hello world!');
+
+// var name = prompt('Enter your name');
+// document.write('Hello ' + name);
+
+// var year = Number(prompt('Year'));
+// var month = Number(prompt('Month'));
+// var day = Number(prompt('Day'));
+
+// var sum =  add(year, month) + day;
+// document.write(sum);
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function getAge(age) {
+//     if (age > 18) {
+//         return 'Adult';
+//     } else {
+//         return 'Child';
+//     }
+// }
+
+// var age = Number(prompt('Enter your age'));
+// var result = getAge(age);
+// document.write(result);
+
+document.write(Math.sqrt(25));
