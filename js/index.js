f4929207ee5fdd915670ea374e8d5efadf3ee87f
Roman Khoronzhak <romix.price.3xd@gmail.com>
2016-12-07 20:50:45 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,31 +1,28 @@
-// alert('Hello world!');
-// document.write('<h1>Hello world!</h1>');
-// console.log('Hello world!');
+var a = Number(prompt('Введіть a'));
+var b = Number(prompt('Введіть b'));
+var c = Number(prompt('Введіть c'));
+var x1;
+var x2;
+var x;
+var result = calculate(a, b, c);
 
-// var name = prompt('Enter your name');
-// document.write('Hello ' + name);
+function calculate(a, b, c) {
+    var dis = b * b - 4 * a * c;
+    if (dis > 0) {
+        x1 = (- b + Math.sqrt(dis)) / (2 * a);
+        x2 = (- b - Math.sqrt(dis)) / (2 * a);
+        return 'x1='+x1+', x2='+x2;
+    } else {
+        if (dis == 0) {
+            x = (- b) / (2 * a);
+            return 'x='+x;
+        } else {
+            if (dis < 0) {
+                return 'D<0';
+            }
+        }
+    }
+}
 
-// var year = Number(prompt('Year'));
-// var month = Number(prompt('Month'));
-// var day = Number(prompt('Day'));
+document.write(result);
 
-// var sum =  add(year, month) + day;
-// document.write(sum);
-
-// function add(a, b) {
-//     return a + b;
-// }
-
-// function getAge(age) {
-//     if (age > 18) {
-//         return 'Adult';
-//     } else {
-//         return 'Child';
-//     }
-// }
-
-// var age = Number(prompt('Enter your age'));
-// var result = getAge(age);
-// document.write(result);
-
-document.write(Math.sqrt(25));
